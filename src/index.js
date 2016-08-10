94d27e08367ac10016aafd90e397d775365961c9
gianfebrian <gianfebrian@gmail.com>
2016-08-10 13:09:31 +0700

    Add entry point

---
+++ src/index.js
@@ -0,0 +1,7 @@
+
+import Amqplib from './amqplib';
+import Connection from './connection'
+import Channel from './channel';
+import hook from './hook';
+
+export default { Amqplib, Connection, Channel, hook };
